c37dcca1053f579d892ce6e16cb533f970f43d66
Thad Sauter <thadsauter@gmail.com>
2017-12-02 10:21:34 -0800

    Added Title, Backstory, and Objective to main function

--- src/Jail_Escape_Main.cpp
+++ src/Jail_Escape_Main.cpp
@@ -11,37 +11,35 @@ int main()
 {
 	std::srand(time(0)); //Starts a seed using the computers internal clock 
 
-	/**
-	Backpack pack;
-	Item* Radio = new Guard_Radio;
-	Item* gun1 = new Gun;
-	Item* spoon1 = new Spoon;
+	//Print the title screen
+	std::cout << std::endl;
+	std::cout << "=========================================================================" << std::endl;
+	std::cout << "--                                                                     --" << std::endl;
+	std::cout << "--                			WELCOME TO JAIL ESCAPE                         --" << std::endl;
+	std::cout << "--                                                                     --" << std::endl;
+	std::cout << "=========================================================================" << std::endl;
+	std::cout << std::endl;
 
-	pack.add_item(Radio);
-	pack.add_item(gun1);
-	pack.add_item(spoon1);
+	std::cout << std::endl;
+	std::cout << "=========================================================================" << std::endl;
+	std::cout << "-- Backstory --                                                        --" << std::endl;
+	std::cout << "---------------                                                        --" << std::endl;
+	std::cout << "-- You are an inmate at a maximum-security prison. It is the middle of --" << std::endl;
+	std::cout << "-- the night and your jail cell mysteriously opens. You peek out into  --" << std::endl;
+	std::cout << "-- the hallway and see that everyone else is asleep. The guard is      --" << std::endl;
+	std::cout << "-- nowhere to be seen ...                                              --" << std::endl;
+	std::cout << "=========================================================================" << std::endl;
 
-	pack.print_list();
+	std::cout << std::endl;
+	std::cout << "=========================================================================" << std::endl;
+	std::cout << "-- Objective --                                                        --" << std::endl;
+	std::cout << "---------------                                                        --" << std::endl;
+	std::cout << "-- To escape, you must use the items you find scattered throughout the --" << std::endl;
+	std::cout << "-- penitentiary. You only have 7 hours before the sun comes up and     --" << std::endl;
+	std::cout << "-- each turn costs you 10 minutes. You better move fast!               --" << std::endl;
+	std::cout << "=========================================================================" << std::endl;
 
-	if(pack.at_capacity())
-	{
-		std::cout << "You already have 3 items in your bag." << std::endl;
-	}
 
-	pack.delete_item(2);
-
-	pack.print_list();
-
-	if(pack.item_in_backpack("Guard Radio"))
-	{
-		std::cout << "Item is in backpack." << std::endl;
-	}
-
-	else
-	{
-		std::cout << "Item is not in backpack." << std::endl;
-	}
-	**/
 
 	Play_Jail_Escape play_game;
 
