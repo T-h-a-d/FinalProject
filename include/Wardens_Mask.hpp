85627efd8c23cc196356bd62fe22ae5fd1e1a3ed
Thad Sauter <thadsauter@gmail.com>
2017-12-02 10:22:44 -0800

    Created new class derived from item, Warden's Mask

---
+++ include/Wardens_Mask.hpp
@@ -0,0 +1,22 @@
+/******************************************************************************************
+** Final Project: Jail Escape
+** Thad Sauter
+** 12/5/17
+** Description: 
+*******************************************************************************************/
+#ifndef WARDENS_MASK_HPP
+#define WARDENS_MASK_HPP
+#include "Item.hpp"
+
+class Wardens_Mask : public Item
+{
+private:
+
+
+
+public:
+	Wardens_Mask();
+
+
+};
+#endif
